565e3b53e268930192733678f48f893982f7d582
Karan Singh <carsings@0587394739.wireless.umich.net>
2016-03-20 14:40:25 -0400

    Initial Commit

---
+++ ssg_code_challenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ssg_code_challenge
+//
+//  Created by Karan Singh on 3/20/16.
+//  Copyright © 2016 Karan Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
